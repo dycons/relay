ac1b700cb96727d5eae59d07d8f0d0a02e3af014
Mic Fok <mic.fok@unity3d.com>
2021-02-11 19:34:16 -0500

    Adds comments

--- app/consents_handler.go
+++ app/consents_handler.go
@@ -9,28 +9,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ProjectConsent is a simple struct that enables the json marshalling and unmarshalling
+// as described by the API
 type ProjectConsent struct {
 	ProjectApplicationID int  `json:"project_application_id"`
 	GeneticConsent       bool `json:"genetic_consent"`
 	ClinicalConsent      bool `json:"clinical_consent"`
 }
 
+// DefaultConsent is a simple struct that enables the json marshalling and unmarshalling
+// as described by the API
 type DefaultConsent struct {
 	GeneticConsent  bool `json:"genetic_consent"`
 	ClinicalConsent bool `json:"clinical_consent"`
 }
 
+// Consents is a simple struct that enables the json marshalling and unmarshalling
+// as described by the API
 type Consents struct {
 	DefaultConsent  DefaultConsent   `json:"default_consent"`
 	ProjectConsents []ProjectConsent `json:"project_consents"`
 }
 
+// ParticipantsRequestHeader is used by gin to
+// bind header information into.
 type ParticipantsRequestHeader struct {
 	AuthorizationHeader string `header:"Authorization"`
 }
 
+// ConsentsGet is the API Handler for responding to GET /consents requests
+// from participants. Extracts the Bearer token and JWT from the
+// Authorization header, decodes and verifies the JWT, and returns
+// a canned consent payload. TODO: This endpoint should
+// lookup the admin from the KeyFile and request the consents from
+// the consents service
 func ConsentsGet(ctx *gin.Context) {
-	jwt, err := ExtractBearerToken(ctx)
+	// Extract Bearer token
+	jwt, err := extractBearerToken(ctx)
 	if err != nil {
 		renderError(
 			http.StatusUnauthorized,
@@ -41,9 +56,9 @@ func ConsentsGet(ctx *gin.Context) {
 	}
 
 	// Decode Bearer Token
-	_, err = ExtractAdminID(jwt)
+	_, err = extractAdminID(jwt)
 	if err != nil {
-		err = fmt.Errorf("Unable to properly decode JWT: %s", err)
+		err = fmt.Errorf("Unable to successfully decode JWT: %s", err)
 		renderError(
 			http.StatusUnauthorized,
 			err,
@@ -52,7 +67,7 @@ func ConsentsGet(ctx *gin.Context) {
 		return
 	}
 
-	// Return Consents
+	// Build canned consents
 	projectConsents := []ProjectConsent{
 		{
 			ProjectApplicationID: 1,
@@ -87,7 +102,7 @@ func renderError(statusCode int, err error, ctx *gin.Context) {
 	})
 }
 
-func ExtractBearerToken(ctx *gin.Context) (*string, error) {
+func extractBearerToken(ctx *gin.Context) (*string, error) {
 	h := ParticipantsRequestHeader{}
 
 	err := ctx.ShouldBindHeader(&h)
--- app/jwt.go
+++ app/jwt.go
@@ -2,12 +2,19 @@ package app
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 
 	jose "github.com/dvsekhvalnov/jose2go"
 )
 
-func ExtractAdminID(jwt *string) (*string, error) {
+// extractAdminID accepts a JWT and returns
+// the adminID inside. It uses the jose2go
+// library to unpack and verify the JWT. It then
+// parses the json payload and verifies that
+// the user's role is research-participant and
+// has an ID present.
+func extractAdminID(jwt *string) (*string, error) {
 	sharedKey := os.Getenv("SHARED_KEY")
 	p, _, err := jose.Decode(*jwt, []byte(sharedKey))
 
@@ -22,9 +29,18 @@ func ExtractAdminID(jwt *string) (*string, error) {
 		return nil, err
 	}
 
-	return &payload.Sub, nil
+	if payload.Role != "research-participant" {
+		return nil, errors.New("subject role must be 'research-participant'")
+	}
+
+	if payload.AdminID == "" {
+		return nil, errors.New("admin id is missing")
+	}
+
+	return &payload.AdminID, nil
 }
 
 type Payload struct {
-	Sub string `json:"sub"`
+	AdminID string `json:"admin_id"`
+	Role    string `json:"role"`
 }
